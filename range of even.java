f7c411f8d30252916e8e85142036a7be0f413675
Yashashwini143 <69624643+Yashashwini143@users.noreply.github.com>
2021-06-28 23:18:29 +0530

    Add files via upload

---
+++ range of even.java
@@ -0,0 +1,36 @@
+import java.util.*;
+public class evenodd {
+    public static void main(String[] args) {
+        int n=50;
+        while(n>=10){
+            System.out.println(n);
+            n=n-2;
+        }
+    }
+    
+}
+
+
+output:
+enter n
+50
+48
+46
+44
+42
+40
+38
+36
+34
+32
+30
+28
+26
+24
+22
+20
+18
+16
+14
+12
+10
